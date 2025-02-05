eb9de61616ac8929f3b91ed63aef248cefbfab13
Root Labs <kylechrisking@gmail.com>
2025-02-05 16:59:39 -0600

    broken
    tried to fix some bugs, ended up breaking things more - old broken project.

--- db/migrate/20170316192252_add_username_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameToUsers < ActiveRecord::Migration
-  def change #anything inside of this method, is translate to SQL code and mofified the database (currently, SQLite)
-  add_column :users, :username, :string
-  add_index :users, :username, unique: true
-  end
-end
